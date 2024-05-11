80606262675b9c0c6e3bd80d8dfc8f186de37024
Rohit <rohityadav787898@gmail.com>
2024-05-11 23:39:18 +0545

    Worked with props and data model

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,12 @@
-import "./App.css";
-
+import Todos from "./components/Todos";
+import Todo from "./models/todo";
 function App() {
-  return <div></div>;
+  const todos = [new Todo("Buy groceries"), new Todo("Learn React")];
+  return (
+    <div>
+      <Todos items={todos} />
+    </div>
+  );
 }
 
 export default App;
